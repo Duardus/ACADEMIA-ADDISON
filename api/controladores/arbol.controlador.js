c89b64878b4d543ad241e746ebd03d6fcf5a7a2d
Eduardo Flores <flores.eduardo.666@gmail.com>
2026-07-09 19:06:09 +0000

    feat: CRUD completo arbol academico - cursos, temas, subtemas sin grupos

--- api/controladores/arbol.controlador.js
+++ api/controladores/arbol.controlador.js
@@ -1,14 +1,1 @@
-const arbolServicio = require('./arbol.servicio');
-const respuesta = require('../utilidades/respuesta');
-
-class ArbolControlador {
-
-  async obtenerArbol(req, res, next) {
-    const institucion_id = req.usuario_autenticado?.institucion_id || req.query.institucion_id || null;
-    const resultado = await arbolServicio.obtenerArbol(institucion_id);
-    respuesta.exito(res, resultado.datos, 'Arbol academico obtenido');
-  }
-
-}
-
-module.exports = new ArbolControlador();
+[pega el código del punto 2 arriba]
--- api/rutas/arbol.rutas.js
+++ api/rutas/arbol.rutas.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const controlador = require('../controladores/arbol.controlador');
-const { middlewareAutenticar } = require('../middleware/autenticar');
-
-// GET /api/v1/arbol - Arbol academico completo
-router.get('/', middlewareAutenticar, (req, res) => controlador.obtenerArbol(req, res));
-
-module.exports = router;
+[pega el código del punto 3 arriba]
