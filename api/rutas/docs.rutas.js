2be67cf4aec9b660c5d09a37aa6c2ba6a7eb9cf1
Eduardo Flores <flores.eduardo.666@gmail.com>
2026-07-14 22:51:53 +0000

    [PHOENIX-AUTO] Backup pre-limpieza raíz (2026-07-14T22:51:53+00:00)

--- api/rutas/docs.rutas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generarDocumentacionViva } = require('../controladores/docs.controlador');
-
-// Documentacion viva - publica, no requiere auth
-router.get('/', generarDocumentacionViva);
-
-module.exports = router;
--- api/rutas/livekit.rutas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { middlewareAutenticar } = require('../middleware/autenticar');
-const { middlewareContexto } = require('../middleware/contexto_institucion');
-const { generarTokenSala } = require('../controladores/livekit.controlador');
-
-router.post('/token', middlewareAutenticar, middlewareContexto, generarTokenSala);
-
-module.exports = router;
